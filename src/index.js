ee439b39da539f651cd6c63fba13cc00b11d222f
Asari Mitsuteru <asari.mtr@gmail.com>
2018-06-17 00:59:59 +0900

    Use two loops to make the squares instead of hardcoding them

--- src/index.js
+++ src/index.js
@@ -23,23 +23,21 @@ class Board extends React.Component {
   }
 
   render() {
+    var rows = [];
+
+    for (var i = 0; i < 3; i++) {
+      var cells = [];
+      for (var j = 0; j < 3; j++) {
+        cells.push(this.renderSquare(i * 3 + j))
+      }
+      rows.push(<div className="board-row">
+        {cells}
+        </div>
+      )
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
